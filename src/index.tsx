1873ab881ed297dce221f7cfa5dc964053a95b0b
OleksandrTheGreat <oleksandr.victorovych.kuzmenko@gmail.com>
2017-10-30 00:00:18 +0200

    init

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>TEST</div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
