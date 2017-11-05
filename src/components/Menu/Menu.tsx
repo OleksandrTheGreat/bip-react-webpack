248329053e33d8685eb6669abd489168abb4ed05
OleksandrTheGreat <oleksandr.victorovych.kuzmenko@gmail.com>
2017-11-06 01:37:56 +0200

    nav on top

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -7,7 +7,7 @@ import {MenuItemList} from './MenuItemList';
 export class Menu extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         <MenuBrandIcon/>
         <MenuToggleButton/>
         <MenuItemList />        
