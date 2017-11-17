cbf30f86a8d38508caefa39ea30cfa7f45c3889a
OleksandrTheGreat <oleksandr.victorovych.kuzmenko@gmail.com>
2017-11-17 10:38:35 +0200

    removed commented

--- src/components/Menu/LanguageBar.tsx
+++ src/components/Menu/LanguageBar.tsx
@@ -15,7 +15,6 @@ export class LanguageBar extends React.Component {
     const en = <img src={require("../../images/gb.gif")}/>;
     const ua = <img src={require("../../images/ua.gif")}/>;
 
-
     return (
       <div className="pull-right">
         <LanguageItem 
@@ -28,32 +27,5 @@ export class LanguageBar extends React.Component {
         />
       </div>
     );
-
-
-    // return (
-    //   <div className="pull-right">
-    //     <a 
-    //       href="#" 
-    //       onClick={() => {bus.Send(new ChangeLanguage(i18n.EN))}}
-    //       className="nav-link"
-    //     >
-    //       <img src={require("../../images/gb.gif")}/>
-    //     </a>;
-    //     <a href="#" onClick={() => {bus.Send(new ChangeLanguage(i18n.UA))}}><img src={require("../../images/ua.gif")}/></a>;
-    //   </div>
-    // );
-
-    // return (
-    //   <ul className="navbar-nav pull-right">
-    //     <MenuItem
-    //       title={en}
-    //       onClick={() => {bus.Send(new ChangeLanguage(i18n.EN))}}
-    //       isActive={false}/>
-    //     <MenuItem
-    //       title={ua}
-    //       onClick={() => {bus.Send(new ChangeLanguage(i18n.UA))}}
-    //       isActive={false}/>
-    //   </ul>
-    // );
   }
 }
