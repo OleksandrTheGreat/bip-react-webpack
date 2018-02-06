7daa391d152fc6ee3f569bdaa049388a867c31c1
OleksandrTheGreat <oleksandr.victorovych.kuzmenko@gmail.com>
2018-02-06 23:08:28 +0200

    db changes

--- src/shared/db.ts
+++ src/shared/db.ts
@@ -1,6 +1,6 @@
 import {GUID} from 'xtypescript';
 import {IDBAdapter, IIDBAdapter} from 'xIndexedDB';
-import {Account, Currency, Marker, Transaction} from '../domain';
+import {Account, Currency, Marker, Transaction, MarkerCategory} from '../domain';
 
 let 
   uahId = GUID.New(),
@@ -22,9 +22,9 @@ let
 
     store.createIndex('name-currency', ['name', 'currencyId'], {unique: true});
     
-    store.add(new Account(GUID.New(), 'Wallet', uahId, 0, null, true, 1));
-    store.add(new Account(GUID.New(), 'Card', uahId, 0, null, true, 2));
-    store.add(new Account(GUID.New(), 'Reserved', uahId, 0, null, true, 3));
+    store.add(new Account(GUID.New(), 'Wallet / Гаманець', uahId, 0, null, true, 1));
+    store.add(new Account(GUID.New(), 'Card / Картка', uahId, 0, null, true, 2));
+    store.add(new Account(GUID.New(), 'Reserved / Резерв', uahId, 0, null, true, 3));
   },
 
   upgradeTransaction = (db : IDBDatabase) => {
@@ -36,6 +36,14 @@ let
 
     let store = db.createObjectStore('Marker', {keyPath: "id"});
     store.createIndex('name-category', ['name', 'category'], {unique: true});
+
+    store.add(new Marker(GUID.New(), MarkerCategory.income, 'Salary / Зарплата'));
+    store.add(new Marker(GUID.New(), MarkerCategory.expense, 'Food / Їжа'));
+    store.add(new Marker(GUID.New(), MarkerCategory.expense, 'Clothes / Одяг'));
+    store.add(new Marker(GUID.New(), MarkerCategory.expense, 'House / Дім'));
+    store.add(new Marker(GUID.New(), MarkerCategory.expense, 'Medicine / Медицина'));
+    store.add(new Marker(GUID.New(), MarkerCategory.expense, 'Transport / Транстпорт'));
+    store.add(new Marker(GUID.New(), MarkerCategory.expense, 'Entertainment / Розваги'));
   };
 
 export const idbAdapter: IIDBAdapter = new IDBAdapter(
